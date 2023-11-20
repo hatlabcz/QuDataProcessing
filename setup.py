f2f04820af8f496fe38044ad9d1b66a09bf8cede
CHAO ZHOU <chz78@pitt.edu>
2023-11-20 17:03:22 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='QuDataProcessing',
+      version='0.0.1',
+      description='Data processing code inherited from https://github.com/PITT-HATLAB/Hatlab_DataProcessing.',
+      packages=['QuDataProcessing'])
